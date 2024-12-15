d793c9cfbf06e98dd97249b1cf61139780177dab
Fredy German <fredgerman30@gmail.com>
2024-12-15 22:43:34 +0300

    feat : added vercel analytics

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import { Analytics } from "@vercel/analytics/next"
 import { type Session } from "next-auth"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
@@ -25,6 +26,7 @@ export default function Providers({
         <NuqsAdapter>
           <TooltipProvider>{children}</TooltipProvider>
         </NuqsAdapter>
+        <Analytics />
         <Toaster richColors />
       </NextThemesProvider>
     </AuthProvider>
